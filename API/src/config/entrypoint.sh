6289354adc14b3d2f9e6a05394bcac4dbbb3d72d
Gustavo Souza <gustavosouza1688@gmail.com>
2024-08-22 14:01:34 -0300

    FEAT: Add entrypoint.sh to configure database in the docker container

---
+++ API/src/config/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+echo 'Waiting for PostgreSQL to start...'
+
+while ! nc -z $DATABASE_HOSTNAME $DATABASE_PORT; do
+    sleep 0.1
+done
+
+echo 'PostgreSQL has started'
+
+echo 'Migrating database...'
+npx prisma migrate deploy
+
+echo 'Generate Prisma Client...'
+npx prisma generate
+
+exec "$@"
